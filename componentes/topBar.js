15478d7df9ec20a5b2dea15ff129d891630d6934
Adler Chaves <adlerchaves@gmail.com>
2025-07-10 21:39:29 -0300

    Cadastro de clientes e fornecedores

--- componentes/topBar.js
+++ componentes/topBar.js
@@ -50,8 +50,8 @@ function renderTopbar(activeMenu, permissions) {
             permission: 'cadastros',
             icon: `<svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>`,
             submenus: [
-                { name: 'Clientes', href: `${pathPrefix}cadastroClientes.html` },
-                { name: 'Fornecedores', href: `${pathPrefix}cadastroFornecedores.html` },
+                { name: 'Clientes', href: `${pathPrefix}clientes.html` },
+                { name: 'Fornecedores', href: `${pathPrefix}fornecedores.html` },
                 { name: 'Usuários', href: `${pathPrefix}cadastroUsuario.html` },
                 { name: 'Categorias', href: `${pathPrefix}cadastroCategorias.html` },
             ] 
